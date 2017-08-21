0b6da643333432ae155f52e50427cc30b2987623
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-08-21 02:22:04 -0500

    Ya compila los archivos en la carpeta app/dist/js

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,28 @@
 var gulp        = require('gulp');
-var renaame     = require('gulp-rename');
+var rename      = require('gulp-rename');
 var browserify  = require('browserify');
 var babelify    = require('babelify');
 var source      = require('vinyl-source-stream');
 var es          = require('event-stream');
 
 gulp.task('build', function(done){
-
     var files = [
-        './app/src/js/app.js',
-        './app/src/js/login.js'
+        'app.js',
+        'login.js'
     ];
-
     var tasks = files.map(function(entry){
-
-        return browserify({entries: entry, debug: true})
+        return browserify({entries: './app/src/js/' + entry, debug: true})
             .transform("babelify", { presets: ["es2015"] })
             .bundle()
             .pipe(source(entry))
-            .pipe(renaame({extname: '.bundle.js'}))
-            .pipe(gulp.dest('./'));
-
+            .pipe(rename({extname: '.bundle.js'}))
+            .pipe(gulp.dest('./app/dist/js'));
     })
     return es.merge.apply(null, tasks);
-
 });
 
 gulp.task('watch', ['build'], function(){
-    
     gulp.watch('./app/src/js/*.js', ['build']);
-
 });
 
 gulp.task('default', ['build','watch']);
