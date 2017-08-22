a7b0739ae02121e73d1ccea84b51857aa81e6f97
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-08-22 01:56:29 -0500

    Se agrega uglify y sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp        = require('gulp');
 var rename      = require('gulp-rename');
 var sass        = require('gulp-sass');
+var uglify      = require('gulp-uglify');
+var sourcemaps  = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
 var browserify  = require('browserify');
 var babelify    = require('babelify');
 var source      = require('vinyl-source-stream');
+var buffer      = require('vinyl-buffer');
 var es          = require('event-stream');
 
 gulp.task('sass', function(){
@@ -23,7 +26,11 @@ gulp.task('js', function(){
             .transform("babelify", { presets: ["es2015"] })
             .bundle()
             .pipe(source(entry))
+            .pipe(buffer())
+            .pipe(sourcemaps.init())
+            .pipe(uglify())
             .pipe(rename({extname: '.bundle.js'}))
+            .pipe(sourcemaps.write('./maps'))
             .pipe(gulp.dest('./app/dist/js'))
             .pipe(browserSync.stream());
     })
